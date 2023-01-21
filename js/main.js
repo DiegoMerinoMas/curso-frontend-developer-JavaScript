1d6c1ceda4f74558bc39d815939a6474e98fc2c9
DiegoMerinoMas <u202111473@upc.edu.pe>
2023-01-20 22:54:18 -0500

    Responisbe de los menus y relacion de los mismos

--- js/main.js
+++ js/main.js
@@ -4,13 +4,31 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const menuIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 
+const menuShoppingCart = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuIcon.addEventListener('click', toggleMobileMenu);
+menuShoppingCart.addEventListener('click', toggleCarAside);
 
 function toggleDesktopMenu() {
+    aside.classList.add('inactive');
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
+    aside.classList.add('inactive');
+    // if(aside.classList.contains('inactive') === false) {
+    //     toggleCarAside();
+    // }
     mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarAside() {
+    desktopMenu.classList.add('inactive');
+    mobileMenu.classList.add('inactive');
+    // if(mobileMenu.classList.contains('inactive') === false) {
+    //     toggleMobileMenu();
+    // }
+    aside.classList.toggle('inactive');
 }
